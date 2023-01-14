ae3be351e4fb278552cab0d72832153ee0efa434
George Davidson <gd240673@gmail.com>
2023-01-14 16:46:42 -0500

    Revert

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
 from pytube import YouTube
 
 while True:
-    command = input("> ")
+    command = input("'help' > ")
 
     if command.lower() == "download" or command.lower() == "dl":
         video_link = input("Video Link > ")
-        video_resolution = input(
-            "Video Resolution 'max', '720p', '480p', '360p', '144p' > ")
 
-        print(video_resolution)
+    try:
+        video = YouTube(command)
 
-        try:
-            video = YouTube(video_link)
+        print(f"Downloading: {video.title}")
 
-            print(f"Downloading: {video.title}")
+            downloaded_video = video.streams.get_highest_resolution()
 
-            if video_resolution.lower() == "max":
-                downloaded_video = video.streams.get_highest_resolution().download("videos")
-            else:
-                downloaded_video = video.streams.filter(
-                    res=video_resolution).first()
-
-            print(downloaded_video)
-            print(type(downloaded_video))
-
-            downloaded_video.download("videos")
+        downloaded_video.download("videos")
 
             print("Successful")
         except:
-            print("Error")
+            print("Unknown Link")
 
     if command.lower() == "help":
         print('''"download" to download a video.
