82177b2765c6c2e5d28af5938c3e3309486d56ed
George Davidson <gd240673@gmail.com>
2023-01-14 16:53:05 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -5,24 +5,13 @@ while True:
 
     if command.lower() == "download" or command.lower() == "dl":
         video_link = input("Video Link > ")
-        video_resolution = input(
-            "Video Resolution 'max', '720p', '480p', '360p', '144p' > ")
-
-        print(video_resolution)
 
         try:
             video = YouTube(video_link)
 
             print(f"Downloading: {video.title}")
 
-            if video_resolution.lower() == "max":
-                downloaded_video = video.streams.get_highest_resolution().download("videos")
-            else:
-                downloaded_video = video.streams.filter(
-                    res=video_resolution).first()
-
-            print(downloaded_video)
-            print(type(downloaded_video))
+            downloaded_video = video.streams.get_highest_resolution()
 
             downloaded_video.download("videos")
 
